66a977d549f7c46d54f752a38a4a02c504842eb8
Henrique Teixeira Oliveira <henriqut@usc.edu>
2016-08-25 00:12:56 -0300

    Top navigation bar + To_Be_Done.txt
    The txt file will be reponsible to guide the developer during the
    website development.
    It will show everything that still need to be done through a list.

--- JS_Kids.js
+++ JS_Kids.js
@@ -6,3 +6,19 @@ E-mail: henriquetcro@gmail.com
 +55(21)981430609
 Rio de Janeiro, Brazil 
 */
+
+
+//Function: navIcon()
+//Purpose: It is responsible to show the menu list when the button is clicked.
+//Parameters: None
+//Returns: Nothing
+function navIcon() {
+    var nav = document.getElementById("myTopnav");  // Get the id of the navegation bar
+    // The button click will change the status of the bar.
+    if (nav.className === "topnav") {       //If the options are hidden
+        nav.className += " responsive";     // It will show all the options as a vertical list
+    }   
+    else {      //If the options are not hidden
+        nav.className = "topnav";   // It will hide all the options
+    }
+}
